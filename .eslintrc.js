4055c5e7d815c70b664fba04e4c9ec7edf1bce1f
KYLN24 <1296845690@qq.com>
2021-03-04 23:31:57 +0800

    format config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "indent": ["error", 4],
-    "quotes": [0, "double"],
-    "semi": [1, "always"],
-    "space-before-function-paren": 0
-  }
-}
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "@vue/standard"],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        indent: ["error", 4],
+        quotes: [0, "double"],
+        semi: [1, "always"],
+        "space-before-function-paren": 0
+    }
+};
